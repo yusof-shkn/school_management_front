aaa112bdcd55148422487137247eff1bea0807d6
Yusof Shokohyan <138888595+yusof-shkn@users.noreply.github.com>
2024-03-26 23:32:46 +0500

    Create Database.jsx

---
+++ src/components/Database.jsx
@@ -0,0 +1,76 @@
+
+import { Toaster,toast } from "sonner";
+
+
+
+function Database() {
+    let token=localStorage.getItem("token");
+    let addDatabase=async()=>{
+
+let getData=await fetch(`https://yusof.pythonanywhere.com/api/database_seeding/?token=${token}`,{
+method: "GET",
+})
+
+let data=await getData.json()
+if(data){
+  toast.success("Database seeded successfully.")
+}else{
+    toast.error("Database seeding failed.")
+}
+    };
+    let removeDatabase=async()=>{
+        let getData=await fetch(`https://yusof.pythonanywhere.com/api/database_delete/?token=${token}`,{
+            method: "GET",
+            })
+            
+            let data=await getData.json()
+            if(data){
+              toast.success("Database deleted successfully.")
+            }else{
+                toast.error("Database delelation failed.")
+            }
+    };
+    return (
+     
+
+<div style={{height:"100vh",width:"100%",display:"flex",justifyContent:"center",alignItems:"center"}}>
+    <Toaster richColors position="top-center"/>
+    <button onClick={()=>{addDatabase()}} style={{
+        width:"100px",
+        height:"40px",
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center",
+        border:"none",
+        outline:"none",
+        cursor:"pointer",
+        backgroundColor:"#000",
+        color:"#fff",
+        borderRadius:"5px",
+        margin:"10px"
+    }}>
+      Add
+    </button>
+        <button onClick={()=>{removeDatabase()}} style={{
+            width:"100px",
+            height:"40px",
+            display:"flex",
+            justifyContent:"center",
+            alignItems:"center",
+            border:"none",
+            outline:"none",
+            cursor:"pointer",
+            backgroundColor:"#000",
+            color:"red",
+            borderRadius:"5px",
+            margin:"10px"
+        }}>
+          Remove
+    </button>
+    </div>
+
+     
+    )
+}
+
+export default Database
